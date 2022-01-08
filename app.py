5f4dadacba13e850a1caf0557a84a47038347107
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2022-01-08 18:57:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import streamlit as st
 
-def get_reddit(subreddit,limit):
+def get_reddit(subreddit, limit, sort_by):
 	base_url = f'https://www.reddit.com/r/{subreddit}.json?limit={limit}'
 	request = requests.get(base_url, headers = {'User-agent': 'yourbot'})
 	r = request.json()['data']['children']
@@ -15,7 +15,7 @@ def get_reddit(subreddit,limit):
 	results = list(zip(title, post, comments, score, url))
 	df = pd.DataFrame(results)
 	df.columns = ["title", "post", "number of comments", "score", "url"]
-	df.sort_values(by="score", ascending=False, inplace=True)
+	df.sort_values(by=sort_by, ascending=False, inplace=True)
 	df = df.reset_index(drop=True)
 	df.index = df.index + 1
 	return df
@@ -25,13 +25,14 @@ st.header("***Reddit Scraper***", anchor=None)
 with st.form(key='my_form'):
 	subreddit = st.text_input(label='Subreddit')
 	limit = st.text_input(label='Limit: 1-100')
+	sort_by = st.selectbox("Sort By", ("Number of Comments", "Score"))
 	submit_button = st.form_submit_button(label='Submit')
 
 if submit_button:
 	if len(subreddit) == 0:
 		st.warning("Please enter a subreddit")		
 	else:
-		df = get_reddit(subreddit, limit)
+		df = get_reddit(subreddit, limit, sort_by)
 		st.table(df)
 		csv = df.to_csv()
 		b64 = base64.b64encode(csv.encode()).decode()

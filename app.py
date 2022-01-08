553978e4db23e7e1f16b899a79b8f8cb27b0dbf4
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2022-01-08 18:33:26 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+import base64
+import pandas as pd
+import requests
+import streamlit as st
+
+def get_reddit(subreddit,limit):
+	base_url = f'https://www.reddit.com/r/{subreddit}.json?limit={limit}'
+	request = requests.get(base_url, headers = {'User-agent': 'yourbot'})
+	r = request.json()['data']['children']
+	title = [element['data']['title'] for element in r]
+	post = [element['data']['selftext'].replace("\n", " ") for element in r]
+	comments = [element['data']['num_comments'] for element in r]
+	score = [element['data']['score'] for element in r]
+	url = ["https://www.reddit.com"+str(element['data']['permalink']) for element in r]
+	results = list(zip(title, post, comments, score, url))
+	df = pd.DataFrame(results)
+	df.columns = ["title", "post", "number of comments", "score", "url"]
+	df.sort_values(by="score", ascending=False, inplace=True)
+	df = df.reset_index(drop=True)
+	df.index = df.index + 1
+	return df
+
+st.set_page_config(layout="wide", page_title="Reddit Scraper")
+st.header("***Reddit Scraper***", anchor=None)
+with st.form(key='my_form'):
+	subreddit = st.text_input(label='Subreddit')
+	limit = st.text_input(label='Limit: 1-100')
+	submit_button = st.form_submit_button(label='Submit')
+
+if submit_button:
+	if len(subreddit) == 0:
+		st.warning("Please enter a subreddit")		
+	else:
+		df = get_reddit(subreddit, limit)
+		st.table(df)
+		csv = df.to_csv()
+		b64 = base64.b64encode(csv.encode()).decode()
+		st.markdown('### **⬇️ Download output CSV File **')
+		href = f"""<a href="data:file/csv;base64,{b64}">Download CSV File</a> (Right-click and save as "filename.csv". Don't left-click.)"""
+		st.markdown(href, unsafe_allow_html=True)
